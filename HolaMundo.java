b3faeb2e921e2fdd51e0e73cb7dfdd646458ad01
newstormer <38796828+newstormer@users.noreply.github.com>
2020-08-30 16:56:57 -0500

    Update HolaMundo.java
    Actualización HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,12 @@
 
 public class HolaMundo {
-public static void main(String[] args) {
-	System.out.println("hola mundo , me llamo Juan Ignacio, me llamo Juan Pablo");
-}
+	static String Holamundo(){
+		System.out.println("hola mundo , me llamo Juan Ignacio, me llamo Juan Pablo");
+		System.out.println("Hola, soy Juan Felipe");
+		return null;
+		
+	}
+	public static void main(String[] args) {
+		Holamundo();
+	}
 }

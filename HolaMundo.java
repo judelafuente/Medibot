ee957adc426c81a61c2ec7f09905f2d9c0f7f9f6
JuanOrtizMerchan <63204462+JuanOrtizMerchan@users.noreply.github.com>
2020-08-31 10:19:36 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,8 @@
 
 public class HolaMundo {
 	static String Holamundo(){
-		System.out.println("hola mundo , me llamo Juan Ignacio, me llamo Juan Pablo");
+		System.out.println("hola mundo , me llamo Juan Ignacio");
+		System.out.println("Hola, soy Juan Pablo");
 		System.out.println("Hola, soy Juan Felipe");
 		System.out.println("Holi, Soy Valentina");
 		return null;

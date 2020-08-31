fa30a82415c1990cb24ce406aa7e6f427f5dc06f
jucalderon10 <69824424+jucalderon10@users.noreply.github.com>
2020-08-31 09:42:49 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
 	static String Holamundo(){
 		System.out.println("hola mundo , me llamo Juan Ignacio, me llamo Juan Pablo");
 		System.out.println("Hola, soy Juan Felipe");
+		System.out.println("Holi, Soy Valentina");
 		return null;
 		
 	}
